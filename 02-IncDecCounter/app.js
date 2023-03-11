205c7937bb6ce31184af1ccdc1848947259a8eb4
unknown <denisbarani0@gmail.com>
2023-03-11 20:37:38 +0100

    Added Colors of numbers when is < 0,>0 and ===

--- 02-IncDecCounter/app.js
+++ 02-IncDecCounter/app.js
@@ -7,15 +7,22 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    const styles =e.currentTarget.classList;
-    if(styles.contains('decrease')){
-        count--;
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
     }
-    else if(styles.contains('increase')){
-        count++;
+    if (count > 0) {
+      value.style.color = "blue";
     }
-    else{
-        count = 0;
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "green";
     }
     value.textContent = count;
   });
